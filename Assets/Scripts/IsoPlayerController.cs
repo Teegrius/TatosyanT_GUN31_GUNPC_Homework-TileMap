092b59308bdc6320c71ee47648b8a9c02afee1e5
Teegrius <tatosian.tigr@yandex.ru>
2026-01-28 22:29:45 +0300

    feat: final commit with README
    - Completed all 3 tilemap scenes
    - Added detailed README.md with project documentation
    - All scenes have functional player movement
    - Tilemap layers properly configured (floor + decorations)
    - Project ready for submission

---
+++ Assets/Scripts/IsoPlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class IsoPlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.gravityScale = 0f;
+            rb.freezeRotation = true;
+        }
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveX, moveY).normalized * speed;
+
+        if (rb != null)
+            rb.velocity = movement;
+        else
+            transform.Translate(movement * Time.deltaTime);
+    }
+}
